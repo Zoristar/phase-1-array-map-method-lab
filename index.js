055523d4f6ad49b3c7b74199c15e43cb6134fbca
Zoriana <zoriana.shatkovska@gmail.com>
2022-11-03 00:38:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((str) => {
+    const words = str.split(" ")
+    const capital = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    const result = capital.join(" ");
+    return result;
+  })
 }
+
